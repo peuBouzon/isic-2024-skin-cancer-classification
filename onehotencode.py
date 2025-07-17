5bb85b3f0b9eb4b4cd84cae89c4d832d14f72793
pedro.bouzon <peubouzon@gmail.com>
2025-07-17 18:48:30 -0300

    remove data leakage on missing data imputation, add StratifientGroupKFold by traget and patient id

--- onehotencode.py
+++ onehotencode.py
@@ -1,28 +1,26 @@
 import os
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.impute import SimpleImputer
 from dataset import ISIC2024
 
-df = pd.read_csv('./train-metadata.csv')
+def fill_nan(train:pd.DataFrame, test:pd.DataFrame) -> pd.DataFrame:
+    imputer = SimpleImputer(strategy='mean')
+    imputer.fit(train[ISIC2024.NUMERICAL_FEATURES])
+    train[ISIC2024.NUMERICAL_FEATURES] = imputer.transform(train[ISIC2024.NUMERICAL_FEATURES])
+    test[ISIC2024.NUMERICAL_FEATURES] = imputer.transform(test[ISIC2024.NUMERICAL_FEATURES])
+    return train, test
 
-for ft in ISIC2024.categorical_features:
-    is_nan = df[ft].isna() | df[ft].isin(['Unknown'])
-    if is_nan.sum() > 0:
-        print(f'Filling missing values for {ft} with mode "{df[ft].mode().values[0]}"...')
-        df.loc[is_nan, ft] = df[ft].mode()
+if __name__ == "__main__":
+    df = pd.read_csv('./train-metadata.csv')
 
-for ft in ISIC2024.numerical_features:
-    is_nan = df[ft].isna()
-    if is_nan.sum() > 0:
-        print(f'Filling missing values for {ft} with mean "{df[ft].mean()}"...')
-        df.loc[is_nan, ft] = df[ft].mean()
+    df = pd.get_dummies(df, columns=ISIC2024.RAW_CATEGORICAL_FEATURES, dtype=int)
+    
+    kfold = StratifiedGroupKFold(n_splits=5, shuffle=True, random_state=42)
 
-df = pd.get_dummies(df, columns=ISIC2024.categorical_features, dtype=int)
+    df[ISIC2024.FOLDER_COLUMN] = None
+    for i, (_, test_indexes) in enumerate(kfold.split(df, df[ISIC2024.TARGET_COLUMN], groups=df[ISIC2024.PATIENT_ID])):
+        df.loc[test_indexes, ISIC2024.FOLDER_COLUMN] = i + 1
 
-kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-df['folder'] = None
-for i, (_, test_indexes) in enumerate(kfold.split(df, df['target'])):
-    df.loc[test_indexes, 'folder'] = i + 1
-
-os.makedirs('./data', exist_ok=True)
-df.to_csv('./data/isic-2024-one-hot-encoded.csv', index=False)
+    os.makedirs('./data', exist_ok=True)
+    df.to_csv('./data/isic-2024-one-hot-encoded.csv', index=False)
