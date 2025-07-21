24196c7f9e543badadcf137370acd95e010d1b13
peubouzon <peubouzon@gmail.com>
2025-07-20 21:46:26 -0300

    update metrics

--- metrics.py
+++ metrics.py
@@ -17,83 +17,107 @@ def get_partial_auc(y_hat, y_true, min_tpr=0.80):
     
     return partial_auc
 
-def plot_precision_recall_curve(probs, y_true, save_path, min_recall=0.8):
-    
+def get_precision_at_recall_95(probs, y_true):
     precision, recall, thresholds = precision_recall_curve(y_true, probs)
-    average_precision = average_precision_score(y_true, probs)
-    
-    plt.figure(figsize=(10, 8))
-    plt.step(recall, precision, color='b', where='post', lw=2, 
-             label=f'Curva de Precisão-Sensibilidade (AP = {average_precision:.4f})')
-    
-    high_recall_idxs = np.where(recall >= min_recall)[0]
-    if len(high_recall_idxs) > 0:
-        high_recall_auc = auc(recall[high_recall_idxs], precision[high_recall_idxs])
-        plt.fill_between(recall[high_recall_idxs], precision[high_recall_idxs], 
-                color='skyblue', alpha=0.5, step='post',
-                label=f'Área com Sensibilidade >= {min_recall}, AUC ≈ {high_recall_auc:.4f})')
-        plt.axvline(x=min_recall, color='r', linestyle='--', 
-                   label=f'Limitar de Sensibilidade = {min_recall}')
     
     target_idxs = np.where(recall >= 0.95)[0]
+    
     if len(target_idxs) > 0:
         target_idx = target_idxs[np.argmax(precision[target_idxs])]
-        target_threshold = thresholds[target_idx]
-        target_precision = precision[target_idx]
-        target_recall_actual = recall[target_idx]
-    plt.plot(target_recall_actual, target_precision, 'ro', markersize=8, 
-            label=f'Limitar = {target_threshold:.4f} (P={target_precision:.4f}, S={target_recall_actual:.4f})')
+        return precision[target_idx]
+    else:
+        raise ValueError()
+
+def plot_precision_recall_curves(methods_names, 
+                                methods_probs, 
+                                methods_labels, 
+                                save_path, 
+                                min_tpr=0.8):
     
+    plt.figure(figsize=(10, 8))
+    colors = [['r', 'lightcoral'], ['g', 'mediumseagreen'], ['b', 'skyblue']]
+    for i, (method_name, probs, y_true, (color, color_fill_between)) in enumerate(zip(methods_names, methods_probs, methods_labels, colors)):
+        precision, recall, thresholds = precision_recall_curve(y_true, probs)
+        average_precision = average_precision_score(y_true, probs)
+        
+        plt.fill_between(recall, precision, 
+                color=color_fill_between, alpha=0.5, step='post')
+        
+        plt.step(recall, precision, color=color, where='post', lw=2, 
+                label=f'Curva PR {method_name}\n(AP = {average_precision:.4f})')
+        
+        target_idxs = np.where(recall >= 0.95)[0]
+        if len(target_idxs) > 0:
+            target_idx = target_idxs[np.argmax(precision[target_idxs])]
+            target_threshold = thresholds[target_idx]
+            target_precision = precision[target_idx]
+            target_recall_actual = recall[target_idx]
+        plt.plot(target_recall_actual, target_precision, f'{color}o', markersize=8, 
+                label=f'P={target_precision:.4f}, TPR={target_recall_actual:.4f}\n(Limiar = {target_threshold:.4f})')
+        
     # Format the plot
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('Sensibilidade (S)')
-    plt.ylabel('Precisão (P)')
-    plt.legend(loc="best")
+    fontsize = 16
+    plt.xlim([0.8, 1.0])
+    plt.ylim([0.0, 0.05])
+    plt.xlabel('Taxa de Verdadeiros Positivos (TPR)', fontsize=fontsize)
+    plt.ylabel('Precisão (P)', fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.legend(loc="best", fontsize=13)
     plt.grid(alpha=0.3)
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
-
-
-def plot_roc_with_partial_auc(probs, y_true, save_path, min_tpr=0.8):
-    fpr, tpr, thresholds = roc_curve(y_true, probs)
-    roc_auc = auc(fpr, tpr)
-   
+    
+def plot_rocs_with_partial_aucs(methods_names, 
+                                methods_probs, 
+                                methods_labels, 
+                                save_path, 
+                                min_tpr=0.8):
+    
     plt.figure(figsize=(10, 8))
-    plt.plot(fpr, tpr, color='b', lw=2, label=f'Curva ROC (AUC = {roc_auc:.4f})')
-    
-    # Destaca a região com TPR >= min_tpr
-    idx = np.where(tpr >= min_tpr)[0]
-    if len(idx) > 0:
-        pAUC = get_partial_auc(probs, y_true, min_tpr=min_tpr)
-        plt.fill_between(fpr[idx], tpr[idx], min_tpr, color='skyblue', alpha=0.5,
-                        label=f'AUC parcial (TPR ≥ {min_tpr}) = {pAUC:.4f}')
-    
-    # Plot a linha de referencia de classificador aleatório
-    plt.plot([0, 1], [0, 1], 'k--', lw=1)
+    colors = [['r', 'lightcoral'], ['g', 'mediumseagreen'], ['b', 'skyblue']]
+    for i, (method_name, probs, labels, (color, color_fill_between)) in enumerate(zip(methods_names, methods_probs, methods_labels, colors)):
+        if i == 0:
+            # Plot a linha de referencia de classificador aleatório
+            plt.plot([0, 1], [0, 1], 'k--', lw=1)
 
-    # Plota uma linha com TPR == min_tpr 
-    plt.axhline(y=min_tpr, color='r', linestyle='--', 
-               label=f'Limitar de TPR mínimo = {min_tpr}')
+            # Plota uma linha com TPR == min_tpr 
+            plt.axhline(y=min_tpr, color='r', linestyle='--', 
+                    label=f'Limiar de TPR mínimo = {min_tpr}')
+            
+        fpr, tpr, thresholds = roc_curve(labels, probs)
+        roc_auc = auc(fpr, tpr)
+        
+        #plt.figure(figsize=(10, 8))
+        plt.plot(fpr, tpr, color=color, lw=2, label=f'ROC {method_name}\n(pAUC @ 80% TPR = {get_partial_auc(probs, labels):.4f})')
+        
+        # Destaca a região com TPR >= min_tpr
+        idx = np.where(tpr >= min_tpr)[0]
+        if len(idx) > 0:
+            pAUC = get_partial_auc(probs, labels, min_tpr=min_tpr)
+            plt.fill_between(fpr[idx], tpr[idx], min_tpr, color=color_fill_between, alpha=0.5)
+        
+        
+        # Marca um ponto referente ao threshold em que o modelo tem pelo menos 0.95 de TPR
+        threshold_tpr = 0.95
+        target_tpr_idx = np.argmin(np.abs(tpr - threshold_tpr))
+        while (target_tpr_idx < len(tpr) - 1 and tpr[target_tpr_idx] < threshold_tpr):
+            target_tpr_idx += 1
+        target_threshold = thresholds[target_tpr_idx]
+        target_fpr = fpr[target_tpr_idx]
+        target_tpr = tpr[target_tpr_idx]
+        plt.plot(target_fpr, target_tpr, f'{color}o', markersize=8, 
+                label=f'TPR={target_tpr:.4f}, FPR={target_fpr:.4f}\n(Limiar = {target_threshold:.4f})')
     
-    # Marca um ponto referente ao threshold em que o modelo tem pelo menos 0.95 de TPR
-    threshold_tpr = 0.95
-    target_tpr_idx = np.argmin(np.abs(tpr - threshold_tpr))
-    while (target_tpr_idx < len(tpr) - 1 and tpr[target_tpr_idx] < threshold_tpr):
-        target_tpr_idx += 1
-    target_threshold = thresholds[target_tpr_idx]
-    target_fpr = fpr[target_tpr_idx]
-    target_tpr = tpr[target_tpr_idx]
-    plt.plot(target_fpr, target_tpr, 'ro', markersize=8, 
-            label=f'Limitar = {target_threshold:.4f} (TPR={target_tpr:.4f}, FPR={target_fpr:.4f})')
-    
+    fontsize = 16
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
-    plt.xlabel('Taxa de Falsos Positivos (FPR)')
-    plt.ylabel('Taxa de Verdadeiros Positivos (TPR)')
-    plt.legend(loc="lower right")
+    plt.xlabel('Taxa de Falsos Positivos (FPR)', fontsize=fontsize)
+    plt.ylabel('Taxa de Verdadeiros Positivos (TPR)', fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.legend(loc="lower right", fontsize=13)
     plt.grid(alpha=0.3)
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
-    
 
 if __name__ == '__main__':
     preds = [0.1, 0.4, 0.35, 0.8, 0.7]
@@ -101,5 +125,3 @@ if __name__ == '__main__':
     preds = np.array(preds)
     labels = np.array(labels)
     print("Partial AUC:", get_partial_auc(preds, labels, min_tpr=0.8))
-    plot_precision_recall_curve(preds, labels, min_recall=0.8)
-    plot_roc_with_partial_auc(preds, labels, min_tpr=0.8)
